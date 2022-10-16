c912004a5e1714db17204c036fcf0098b20195f3
jjd1990 <jack.douglas@rocketmail.com>
2022-10-16 21:05:08 +0100

    spree homepage set back to standard

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # get 'pages/home'
-  # root 'pages#home'
   # This line mounts Solidus's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/store'
+  mount Spree::Core::Engine, at: '/'
 
   root to: 'home#index'
 
